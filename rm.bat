7f091063c04afacdb053d77ae46d37129555d4b4
Starlight0798 <244057735@qq.com>
2024-06-17 00:54:59 +0800

    Update rm.bat

--- rm.bat
+++ rm.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 set "currentDir=%~dp0"
-for /r "%currentDir%" %%d in (log) do (
+for /r "%currentDir%" %%d in (log __pycache__) do (
     if exist "%%d" (
         rd /s /q "%%d"
     )
