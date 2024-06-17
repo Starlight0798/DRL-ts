fc1b0be7868feafc5a3384948b98f432c41826a7
Starlight0798 <244057735@qq.com>
2024-06-17 14:42:06 +0800

    feat: raise for logging.warning

--- utils/handler.py
+++ utils/handler.py
@@ -1,5 +1,6 @@
 from loguru import logger
-
+import logging
+import sys
 
 def raise_warning():
     '''
@@ -8,13 +9,20 @@ def raise_warning():
     '''
     import warnings
     
-    def warning_handler(message, category, filename, lineno, file=None, line=None):
-        with logger.catch(reraise=False):
+    def warning_handler(message, category, filename, lineno, file, line=None):
+        with logger.catch():
             raise Exception(f"{filename}:{lineno}: {category.__name__}: {message}")
         logger.error('[DEBUG] Process killed because of warning!')
-        exit(0)
+        sys.exit(0)
+        
+    def logger_handler(*args):
+        with logger.catch():
+            raise Exception(*args)
+        logger.error('[DEBUG] Process killed because of Logging warning!')
+        sys.exit(0)
         
     warnings.showwarning = warning_handler
+    logging.Logger.warning = logger_handler
     
     
     
