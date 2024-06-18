c7d3ea92b8e47b34498f1127e7c9d873b9069359
Starlight0798 <244057735@qq.com>
2024-06-18 11:41:59 +0800

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-find . -type d -name "log" -exec rm -rf {} +
-find . -type d -name "__pycache__" -exec rm -rf {} +
+find . -type d -name log -exec rm -fr "{}" \;
+find . -type d -name __pycache__ -exec rm -fr "{}" \;
 echo "All log and __pycache__ directories have been deleted."
