4f66ae9af130bda990c3fd425733aacd0b2cbff1
Starlight0798 <244057735@qq.com>
2024-06-23 15:38:42 +0800

    Hyper

--- atari/Mspacman.py
+++ atari/Mspacman.py
@@ -51,7 +51,7 @@ def get_args() -> argparse.Namespace:
     parser.add_argument("--task", type=str, default="MsPacmanNoFrameskip-v4")
     parser.add_argument("--seed", type=int, default=4213)
     parser.add_argument("--scale-obs", type=int, default=1)
-    parser.add_argument("--buffer-size", type=int, default=200000)
+    parser.add_argument("--buffer-size", type=int, default=100000)
     parser.add_argument("--actor-lr", type=float, default=1e-4)
     parser.add_argument("--critic-lr", type=float, default=5e-4)
     parser.add_argument("--gamma", type=float, default=0.99)
